bd741d751e1ab7492c6e697158b812e348703249
Bing Xie <bing708@gmail.com>
2015-07-11 15:41:47 +1000

    Import category API [skip ci]

--- app/services/listing_service/api/categories.rb
+++ app/services/listing_service/api/categories.rb
@@ -13,5 +13,29 @@ module ListingService::API
         CategoryStore.create(community_id: community_id, opts: opts)
       )
     end
+
+    # create_category!(community_id: 10, name: "Cars", listing_shape_ids: [1])
+    def create_category!(community_id:, parent_id: nil, name:, listing_shape_ids: [])
+      community = Community.find(community_id)
+      sort_priority = Admin::SortingService.next_sort_priority(community.categories)
+
+      category_opts = {
+        parent_id: parent_id,
+        sort_priority: sort_priority,
+        basename: name,
+        translations: [{name: name, locale: "zh"}]
+      }
+
+      res = self.create(community_id: community_id, opts: category_opts)
+      cat_id = res.data[:id]
+
+      # Create association
+      listing_shape_ids.each { |lsid|
+        # FIXME Do not use Models to initialize test data for API tests!
+        CategoryListingShape.create!(category_id: cat_id, listing_shape_id: lsid)
+      }
+
+      cat_id
+    end
   end
 end
